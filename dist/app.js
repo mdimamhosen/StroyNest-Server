fc7a3c2a8424dc0424ebb8d8c283fe76bad5500b
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-02-27 16:13:09 +0600

    StoryNest Book Shop Sever

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,7 @@ const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const routes_1 = require("./app/routes");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:5173',
+    origin: 'https://srorynest-omega.vercel.app',
     credentials: true,
 }));
 app.use(express_1.default.json({ limit: '50mb' }));
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { routes } from './app/routes';
 const app: Application = express();
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://srorynest-omega.vercel.app',
     credentials: true,
   }),
 );
