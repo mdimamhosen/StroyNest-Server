f946edddbd364042b17673ace34b890d7ef80a03
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-02-27 15:48:44 +0600

    StoryNest Book Shop Sever

--- dist/app/utils/sendImageToCloudinary.js
+++ dist/app/utils/sendImageToCloudinary.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.upload = exports.uploadImageToCloudinary = void 0;
+/* eslint-disable @typescript-eslint/no-explicit-any */
 const cloudinary_1 = require("cloudinary");
 const config_1 = __importDefault(require("../config"));
 const multer_1 = __importDefault(require("multer"));
@@ -13,6 +14,7 @@ cloudinary_1.v2.config({
     api_key: config_1.default.apiKey,
     api_secret: config_1.default.apiSecret,
 });
+// Upload Image to Cloudinary
 const uploadImageToCloudinary = (path, public_id, folderName) => {
     return new Promise((resolve, reject) => {
         cloudinary_1.v2.uploader.upload(path, { folder: folderName, public_id }, function (error, result) {
@@ -20,6 +22,7 @@ const uploadImageToCloudinary = (path, public_id, folderName) => {
                 return reject(error);
             }
             resolve(result);
+            // Delete file from local storage after upload
             fs_1.default.unlink(path, err => {
                 if (err) {
                     console.error('Error removing file:', err);
@@ -36,13 +39,28 @@ const storage = multer_1.default.diskStorage({
     destination: function (req, file, cb) {
         const uploadPath = process.cwd() + '/uploads';
         if (!fs_1.default.existsSync(uploadPath)) {
-            fs_1.default.mkdirSync(uploadPath, { recursive: true }); // Ensure directory exists
+            fs_1.default.mkdirSync(uploadPath, { recursive: true });
         }
         cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + '-' + uniqueSuffix);
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]);
     },
 });
-exports.upload = (0, multer_1.default)({ storage: storage });
+// File Filter to Allow Only Images
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+    }
+    else {
+        cb(new Error('Only images are allowed!'), false);
+    }
+};
+exports.upload = (0, multer_1.default)({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // Limit file size to 5MB
+    },
+});
